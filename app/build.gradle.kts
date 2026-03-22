30f34d3befe84e873df8e150d675d6e98f436e2a
sai-charan2003 <saicharannallapaneni@gmail.com>
2026-03-21 22:10:32 -0400

    chore: bump version to 0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.charan.habitdiary"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.8.6"
+        versionCode = 13
+        versionName = "0.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
