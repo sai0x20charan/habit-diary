3a602e55b7ad163df75de221ee1e15c222b82e56
sai-charan2003 <saicharannallapaneni@gmail.com>
2026-06-30 19:20:56 -0400

    chore: update github link

--- app/src/main/java/com/charan/habitdiary/core/utils/Constants.kt
+++ app/src/main/java/com/charan/habitdiary/core/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.charan.habitdiary.core.utils
 
 
-val GITHUB_URL = "https://www.github.com/sai-charan2003/Habit-Diary"
+val GITHUB_URL = "https://www.github.com/sai0x20charan/habit-diary"
 val PLAY_STORE_URL = "https://play.google.com/store/apps/details?id=com.charan.habitdiary"
 
 val DEVELOPER_EMAIL = "apps.bycharan@gmail.com"
