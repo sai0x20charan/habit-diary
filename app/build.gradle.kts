6018235f202deb24c91968e3c1fc8e1f74f8e4a4
sai-charan2003 <saicharannallapaneni@gmail.com>
2026-07-14 15:42:44 -0400

    chore: bump version to 0.14.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.charan.habitdiary"
         minSdk = 26
         targetSdk = 37
-        versionCode = 21
-        versionName = "0.13.1"
+        versionCode = 22
+        versionName = "0.14.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
